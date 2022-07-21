90424596f03d098e8681dbfbf16cf30f12bc9103
ThomCode95 <thomas.arias95@gmail.com>
2022-07-21 17:08:04 -0500

    testing deployment

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ function App() {
           >
             Learn React
           </a>
+          <p>Hola amigos</p>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
